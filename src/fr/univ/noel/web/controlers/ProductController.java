b0d443e31b988d6632c36ffbc20b59f521c3d7a0
Florian <flo.campanella@gmail.com>
2017-01-17 11:30:59 +0100

    test

--- src/fr/univ/noel/web/controlers/ProductController.java
+++ src/fr/univ/noel/web/controlers/ProductController.java
@@ -42,7 +42,7 @@ public class ProductController {
 		session.setAttribute("product", productManager.getProduct(productID));
 		
 		logger.info("Returning show_product view");
-		return "show_product"
+		return "show_product";
 	}
 	
 	
